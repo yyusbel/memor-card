fa3b2f92099d3d0ec740ab08a9c25217968f8084
Yusuf <yyusbel@yahoo.com>
2022-08-02 14:58:27 +0100

    Initial commit

---
+++ memor-card/src/theme/Theme.js
@@ -0,0 +1,15 @@
+export const Theme = {
+    colors: {
+      light: '#eee',
+      lightHover: '#ccc',
+      lightActive: '#aaa',
+      dark: '#383838',
+      darkHover: '#272727',
+      darkActive: '#161616',
+      green: '#b5ffb5',
+      red: '#ffb5b5',
+      pokeballRed: '#ef0e3d',
+    },
+  }
+  
+  export default Theme
